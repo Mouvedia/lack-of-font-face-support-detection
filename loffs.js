cb18b2839f58bd99da1879333e07a4efb40a1fb6
Mouvedia <info@mouvedia.com>
2010-02-23 05:23:02 +0100

    first commit

---
+++ loffs.js
@@ -0,0 +1,3 @@
+if (
+(((opera && opera.toString()=="[object Opera]")||(/a/.__proto__=='//')||('MozOpacity' in document.body.style))&&(!document.querySelector))||(m=/*@cc_on!@*/0)||(((/source/.test(/a/.toString+''))||(window.chrome))&&(!window.openDatabase))
+)
